45e0485c1a0aea52ae380b2415c0383e41f20791
Kostadina Geceska <kostadinag@nextsense.com>
2024-06-24 14:01:54 +0200

    inital commit

---
+++ HelloWorld.framework/Headers/HelloWorld.h
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by Kostadina Gecevska on 24.6.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloWorld.
+FOUNDATION_EXPORT double HelloWorldVersionNumber;
+
+//! Project version string for HelloWorld.
+FOUNDATION_EXPORT const unsigned char HelloWorldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloWorld/PublicHeader.h>
+
+
---
+++ HelloWorld/HelloTest.swift
@@ -0,0 +1,19 @@
+//
+//  HelloTest.swift
+//  HelloWorld
+//
+//  Created by Kostadina Gecevska on 24.6.24.
+//
+
+import Foundation
+
+
+public class HelloWorld {
+    let hello = "Hello"
+
+    public init() {}
+
+    public func hello(to whom: String) -> String {
+        return "Hello \(whom)"
+    }
+}
